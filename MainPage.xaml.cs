a9c5ef3f4647055e0c9262088eac218854f1f1e9
Jesterby <jester.by@mail.ru>
2015-12-23 20:19:12 +0300

    Новое

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -26,10 +26,90 @@ namespace App3
         {
             this.InitializeComponent();
         }
-
-        private void inputButton_Click(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            greetingOutput.Text = "ВОТЧ МИ,";
+            Helpers.CreateHttpClient(ref httpClient);
+            cts = new CancellationTokenSource();
         }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // If the navigation is external to the app do not clean up.
+            // This can occur on Phone when suspending the app.
+            if (e.NavigationMode == NavigationMode.Forward && e.Uri == null)
+            {
+                return;
+            }
+
+            Dispose();
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                Uri resourceAddress;
+
+                // The value of 'AddressField' is set by the user and is therefore untrusted input. If we can't create a
+                // valid, absolute URI, we'll notify the user about the incorrect input.
+                if (!Helpers.TryGetUri(AddressField.Text, out resourceAddress))
+                {
+                    rootPage.NotifyUser("Invalid URI.", NotifyType.ErrorMessage);
+                    return;
+                }
+
+                Helpers.ScenarioStarted(StartButton, CancelButton, OutputField);
+                rootPage.NotifyUser("In progress", NotifyType.StatusMessage);
+
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(resourceAddress).AsTask(cts.Token);
+
+                    await Helpers.DisplayTextResultAsync(response, OutputField, cts.Token);
+                    response.EnsureSuccessStatusCode();
+
+                    XElement element = XElement.Parse(await response.Content.ReadAsStringAsync().AsTask(cts.Token));
+                    OutputList.ItemsSource = (
+                        from c in element.Elements("item")
+                        select c.Attribute("name").Value);
+
+                    rootPage.NotifyUser("Completed", NotifyType.StatusMessage);
+                }
+                catch (TaskCanceledException)
+                {
+                    rootPage.NotifyUser("Request canceled.", NotifyType.ErrorMessage);
+                }
+                catch (Exception ex)
+                {
+                    rootPage.NotifyUser("Error: " + ex.Message, NotifyType.ErrorMessage);
+                }
+                finally
+                {
+                    Helpers.ScenarioCompleted(StartButton, CancelButton);
+                }
+            }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
+            cts.Dispose();
+
+            // Re-create the CancellationTokenSource.
+            cts = new CancellationTokenSource();
+        }
+
+        public void Dispose()
+        {
+            if (httpClient != null)
+            {
+                httpClient.Dispose();
+                httpClient = null;
+            }
+
+            if (cts != null)
+            {
+                cts.Dispose();
+                cts = null;
+            }
+        }
+    }
     }
 }
